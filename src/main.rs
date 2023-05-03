af55b07553b5d51fa0f01875a50525b15b03356e
Phodal Huang <h@phodal.com>
2023-05-03 15:42:42 +0800

    feat: add ploaris add data load

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 pub mod instruction;
 pub mod errors;
 
+use std::fs::File;
 use std::path::PathBuf;
 use std::sync::Arc;
 use druid::widget::{Button, Flex, Label, TextBox};
-use druid::{
-    AppDelegate, AppLauncher, Data, DelegateCtx, Lens, LocalizedString, PlatformError,
-    Widget, WidgetExt, WindowDesc, commands, Command, Env, FileDialogOptions, FileSpec, Handled, Target,
-};
+use druid::{AppDelegate, AppLauncher, Data, DelegateCtx, Lens, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc, commands, Command, Env, FileDialogOptions, FileSpec, Handled, Target, UnitPoint};
+use polars::frame::DataFrame;
+use polars::prelude::{JsonFormat, JsonWriter, SerWriter, JsonReader, JsonLineReader, SerReader};
 use crate::instruction::Instruction;
 
 #[derive(Clone, Data, Lens)]
 struct AppState {
-    demo: String,
+    count: String,
     #[data(eq)]
     instructions: Vec<Instruction>,
     #[data(ignore)]
-    file_path: Option<PathBuf>,
+    df: Arc<DataFrame>,
 }
 
 struct Delegate;
@@ -25,9 +25,9 @@ fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder()).title("InsViewer");
 
     let initial_state = AppState {
-        demo: "Type here".to_string(),
+        count: 0.to_string(),
         instructions: Vec::new(),
-        file_path: None,
+        df: Arc::new(DataFrame::default()),
     };
 
     AppLauncher::with_window(main_window)
@@ -36,18 +36,31 @@ fn main() -> Result<(), PlatformError> {
         .launch(initial_state)
 }
 
+
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const TEXT_BOX_WIDTH: f64 = 200.0;
+
 fn ui_builder() -> impl Widget<AppState> {
-    let instruction = TextBox::new()
-        .with_placeholder("Enter instruction")
-        .lens(AppState::demo)
-        .expand_width();
+    let label = Label::new(|data: &AppState, _env: &Env| format!("Count: {}", data.count))
+        .with_text_size(32.0);
+
+    //
+    // let count = TextBox::new()
+    //     .with_placeholder("Count")
+    //     .lens(AppState::count)
+    //     .expand_width();
+
+    // Flex::column()
+    //     .with_flex_child(
+    //         Flex::row()
+    //             .with_child(instruction),
+    //         1.0,
+    //     )
 
     Flex::column()
-        .with_flex_child(
-            Flex::row()
-                .with_child(instruction),
-            1.0,
-        )
+        .with_child(label)
+        .with_spacer(VERTICAL_WIDGET_SPACING)
+        .align_vertical(UnitPoint::CENTER)
 }
 
 // TODO: modify this to save the instruction to a file
@@ -61,14 +74,31 @@ impl AppDelegate<AppState> for Delegate {
         _env: &Env,
     ) -> Handled {
         if let Some(file_info) = cmd.get(commands::SAVE_FILE_AS) {
-            todo!("Save the instruction to a file");
+            // let mut file = File::open(file_info.path()).unwrap();
+            // JsonWriter::new(&mut file)
+            //     .with_json_format(JsonFormat::JsonLines)
+            //     .finish(&mut *data.df)
+            //     .unwrap()
         }
         if let Some(file_info) = cmd.get(commands::OPEN_FILE) {
-            match file_info.path().extension() {
+            let mut file = File::open(file_info.path()).unwrap();
+            match file_info.path.extension() {
                 Some(ext) => {
                     match ext.to_str() {
-                        Some("json") => {}
-                        Some("jsonl") => {}
+                        Some("json") => {
+                            use polars::prelude::*;
+
+                            let df = JsonReader::new(&mut file).finish().unwrap();
+                            data.count = df.height().to_string();
+                            data.df = Arc::new(df);
+
+                            println!("{:?}", data.count)
+                        }
+                        Some("jsonl") => {
+                            let df = JsonLineReader::new(&mut file).finish().unwrap();
+                            data.count = df.height().to_string();
+                            data.df = Arc::new(df);
+                        }
                         _ => println!("Invalid file type"),
                     }
                 }
